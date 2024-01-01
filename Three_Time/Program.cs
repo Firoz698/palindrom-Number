b0194fa34b515998ad06198910c41442c432a96b
Itz Firoz <mdfirozahemd698@gmail.com>
2024-01-02 01:39:38 +0600

    palindrom Number Complate Update

--- Three_Time/Program.cs
+++ Three_Time/Program.cs
@@ -1,2 +1,38 @@
 ﻿//Palindrome Number in C# 
-Console.WriteLine("Practice palindrom Number ;");
+//a Palindrom number is a number that remains the same when its
+//digits are reversed
+Console.Write("Plase Enter Your Numebr : ");
+int number = Convert.ToInt32( Console.ReadLine());
+
+//int number = 121;
+int rem;
+int result = 0;
+int temp = number;
+
+while (number!=0)
+{
+    rem = number % 10;
+    result = result * 10 + rem;
+    number = number / 10;
+}
+if (temp == result)
+{
+    Console.WriteLine($" {temp} Palindrome Number");
+}
+else
+{
+    Console.WriteLine($" {temp} Is Not a Palindrom Number");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
